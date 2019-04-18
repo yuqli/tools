b12d9ddde74577ff724d5ac63bfb8bc54d034a94
yuqli <greenstone1564@gmail.com>
2019-04-18 14:20:36 -0400

    more tools

---
+++ check.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pptk
+
+# root = "/media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample"
+root = "/media/yuqiong/DATA/city/zurich/test_pcds"
+all_f = os.listdir(root)
+
+for i in range(10):
+    path = os.path.join(root, all_f[i])
+    p = np.load(path)
+    v = pptk.viewer(p)
