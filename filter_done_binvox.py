93b0077f256ab90b78225e0eb31a1a29f409f880
yuqli <greenstone1564@gmail.com>
2019-04-24 21:23:36 -0400

    add local files

---
+++ filter_done_binvox.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+### The situation is we did mesh to binvox on NYC data but the program crashed halfway
+### So some meshes are done while others undone
+### There needs a way to filter finished meshes from unfinished meshes
+### We decide that instead of moving things elsewhere, we'll just write the meshes that are not yet done to a txt file
+
+import os
+
+def list2file(l, name):
+    """
+    output a list to file.
+    """
+    with open(name+".txt", 'a+') as f:
+        for item in l:
+            f.write("%s\n" % item)
+    return
+
+parent_dir = "/data/city/nyc/nyc_poly_objs"
+all_files = os.listdir(parent_dir)
+all_objs = [x for x in all_files if x[-4:] == ".obj"]
+all_binvox = [x for x in all_files if x[-4:] != ".obj"]
+unfinished_objs = [x for x in all_objs if x.split(".")[0]+".binvox" not in all_binvox]
+
+list2file(unfinished_objs, "undone_objs")
+
