f35cd16a936d45ca22396c97e6eddb6b8684ec1e
yuqli <greenstone1564@gmail.com>
2019-04-24 22:32:57 -0400

    add zurich file

---
+++ filter_done_binvox_zurich.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+### The situation is we did mesh to binvox on NYC data but the program crashed halfway
+### So some meshes are done while others undone
+### There needs a way to filter finished meshes from unfinished meshes
+### We decide that instead of moving things elsewhere, we'll just write the meshes that are not yet done to a txt file
+
+import os
+
+def list2file(l, path):
+    """
+    output a list to file.
+    """
+    with open(path, 'a+') as f:
+        for item in l:
+            f.write("%s\n" % item)
+    return
+
+parent_dir = "/data/city/zurich/zurich_new_objs"
+all_files = os.listdir(parent_dir)
+all_objs = [x for x in all_files if x[-4:] == ".obj"]
+print("Finished counting all objs!")
+all_binvox = [x for x in all_files if x[-4:] != ".obj"]
+print("Finished counting all binvox!")
+unfinished_objs = [x for x in all_objs if x.split(".")[0]+".binvox" not in all_binvox]
+print("Finished counting all done objs!")
+
+save_dir = "/data/city/zurich"
+list2file(unfinished_objs, os.path.join(save_dir, "undone_objs.txt"))
