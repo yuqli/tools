d78cf683c762ddb41af27672779b1243fe1e0dda
yuqli <greenstone1564@gmail.com>
2019-04-30 12:36:55 -0400

    resolute binvox script

--- viewpcd.py
+++ viewpcd.py
@@ -2,13 +2,23 @@ import numpy as np
 import os
 import pptk
 
-src = "/media/yuqiong/DATA/city/nyc/nyc_tri_pcds_sample"
-allf = os.listdir(src)
+# p1 = "/media/yuqiong/DATA/city/nyc/nyc_tri_pcds_sample_2048"
+p2 = "/media/yuqiong/DATA/city/nyc/nyc_tri_pcds_4096_sample"
 
-for f in allf[41:]:
+# allf1 = os.listdir(p1)
+allf2 = os.listdir(p2)
+# allf = list(set(allf1) & set(allf2))
+
+for f in allf2:
     print(f)
-    path = os.path.join(src, f)
+    path = os.path.join(p2, f)
     dat = np.load(path)
     v = pptk.viewer(dat)
     input("Press Enter to continue...")
     v.close()
+
+    # path = os.path.join(p2, f)
+    # dat = np.load(path)
+    # v = pptk.viewer(dat)
+    # input("Press Enter to continue...")
+    # v.close()
